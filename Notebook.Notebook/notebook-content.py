93ea7eeccad470a1cd9a3ab55c8ca091beed6888
patriciotorres-creai <patriciotorres@creai.mx>
2026-03-09 22:45:08 -0500

    Committing 3 items from workspace 1c56a123-2034-4474-8fc5-022e8f14ef59

--- Notebook.Notebook/notebook-content.py
+++ Notebook.Notebook/notebook-content.py
@@ -8,12 +8,12 @@
 # META   },
 # META   "dependencies": {
 # META     "lakehouse": {
-# META       "default_lakehouse": "02e576ba-909d-406a-b8a3-06cbb92d5778",
-# META       "default_lakehouse_name": "dev_lake",
-# META       "default_lakehouse_workspace_id": "ccf17eb3-09d8-4ed2-a3cd-2965db867334",
+# META       "default_lakehouse": "9b4b7f60-8c7d-4dc4-887e-3bf8d5cd5279",
+# META       "default_lakehouse_name": "stage_lake",
+# META       "default_lakehouse_workspace_id": "1c56a123-2034-4474-8fc5-022e8f14ef59",
 # META       "known_lakehouses": [
 # META         {
-# META           "id": "02e576ba-909d-406a-b8a3-06cbb92d5778"
+# META           "id": "9b4b7f60-8c7d-4dc4-887e-3bf8d5cd5279"
 # META         }
 # META       ]
 # META     },
