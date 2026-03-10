f49021473fd0ce7330346013156bf60bef12e58a
patriciotorres-creai <patriciotorres@creai.mx>
2026-03-09 22:42:19 -0500

    Committing 2 items from workspace ccf17eb3-09d8-4ed2-a3cd-2965db867334

---
+++ Notebook.Notebook/notebook-content.py
@@ -0,0 +1,50 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "02e576ba-909d-406a-b8a3-06cbb92d5778",
+# META       "default_lakehouse_name": "dev_lake",
+# META       "default_lakehouse_workspace_id": "ccf17eb3-09d8-4ed2-a3cd-2965db867334",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "02e576ba-909d-406a-b8a3-06cbb92d5778"
+# META         }
+# META       ]
+# META     },
+# META     "warehouse": {}
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+print("This is a change")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+print("This is a new code")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# This is a New Description
